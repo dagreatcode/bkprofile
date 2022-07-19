6404cd83af4fb754b82ead9819d23020632d85c3
dagreatcode <dagreatcode@gmail.com>
2022-07-19 01:21:02 +0100

    added lint to the main and dev branch

--- server.js
+++ server.js
@@ -7,52 +7,52 @@ const PORT = process.env.PORT || 3001;
 
 const app = express();
 
-app.use(express.urlencoded({ extended: true })); 
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
-	app.use(express.static("client/build"));
+  app.use(express.static("client/build"));
 }
 mongoose.connect(
-	process.env.MONGODB_URI || "mongodb://localhost/bkprofile_db",
-	{
-		useNewUrlParser: true,
-		useUnifiedTopology: true,
-		// useCreateIndex: true,
-		// useFindAndModify: false,
-		// onAfterSetupMiddleware: true,
-		// onBeforeSetupMiddleware: true,
-	}
+  process.env.MONGODB_URI || "mongodb://localhost/bkprofile_db",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    // useCreateIndex: true,
+    // useFindAndModify: false,
+    // onAfterSetupMiddleware: true,
+    // onBeforeSetupMiddleware: true,
+  }
 );
 
 const connection = mongoose.connection;
 
 connection.on("connected", () => {
-	console.log("Mongoose successfully connected.");
+  console.log("Mongoose successfully connected.");
 });
 
 connection.on("error", (err) => {
-	console.log("Mongoose connection error: ", err);
+  console.log("Mongoose connection error: ", err);
 });
 
 app.get("/api/config", (req, res) => {
-	res.json({
-		success: true,
-	});
+  res.json({
+    success: true,
+  });
 });
 
 app.get("/apiFun", (req, res) => {
-	res.send("API FUN");
-	var adminUser = req.params.apiFun;
-	console.log(adminUser);
-	res.end();
+  res.send("API FUN");
+  var adminUser = req.params.apiFun;
+  console.log(adminUser);
+  res.end();
 });
 
 app.get("*", (req, res) => {
-	res.sendFile(path.join(__dirname, "./client/build/index.html"));
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
 
 app.listen(PORT, () => {
-	console.log(`🌎 App is running on http://localhost:${PORT}`);
+  console.log(`🌎 App is running on http://localhost:${PORT}`);
 });

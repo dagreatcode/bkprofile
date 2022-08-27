067054c447bb48ef4e48d6719c9e63934d427c90
Vincent Kendrick <dagreatcode@gmail.com>
2022-08-27 01:11:52 -0400

    added footer

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+// import React, { useEffect, useState } from "react";
 // import AlertContext from "./utils/ContextAPI/AlertContext";
 import {
   BrowserRouter as Router,
@@ -16,21 +16,25 @@ import NavBar from "./components/NavBar/NavBar";
 import NotFound from "./containers/NotFound/NotFound";
 // import AuthContext from "./utils/ContextAPI/AuthContext";
 // import Alert from "./components/Alert";
+import Footer from "./components/Footer/Footer";
 
 function App() {
   return (
-    <div className="App">
-      <Router>
-        <NavBar />
-        <Routes>
-          <Route exact path="/About" element={<About />} />
-          <Route exact path="/Profile" element={<Profile />} />
-          <Route exact path="/Contact" element={<Contact />} />
-          <Route exact path="/" element={<Home />} />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </Router>
-    </div>
+    <>
+      <div className="App">
+        <Router>
+          <NavBar />
+          <Routes>
+            <Route exact path="/About" element={<About />} />
+            <Route exact path="/Profile" element={<Profile />} />
+            <Route exact path="/Contact" element={<Contact />} />
+            <Route exact path="/" element={<Home />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+          <Footer />
+        </Router>
+      </div>
+    </>
   );
 }
 

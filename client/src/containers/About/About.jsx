6d6747ea947a19f333b38e8d7b2c6ce535ede1c8
Vincent Kendrick <dagreatcode@gmail.com>
2022-08-27 09:10:28 -0400

    added footer

--- client/src/containers/About/About.jsx
+++ client/src/containers/About/About.jsx
@@ -1,13 +1,27 @@
-import React, { useState, useEffect } from "react";
+// import React, { useState, useEffect } from "react";
+import profileImg from "../landing.png"
 
 export default function About() {
-  const [useMyState, setMyUseState] = useState();
-
-  useEffect(() => {}, []);
+  // const [useMyState, setMyUseState] = useState();
+  // useEffect(() => {}, []);
 
   return (
     <>
-      <h1 className="text-color">About Me</h1>
+      <div className="text-color cardStyle">
+        <div class="card">
+          <img src={profileImg} class="card-img-top" alt="Barbara Kendrick" />
+          <div class="card-body">
+            <h5 class="card-title">Here is a little about me...</h5>
+            <p class="card-text">
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </p>
+            <a href="/" class="btn btn-primary">
+              Go somewhere
+            </a>
+          </div>
+        </div>
+      </div>
     </>
   );
 }

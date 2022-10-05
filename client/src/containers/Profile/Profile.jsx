988d6384cc10146b9e20fb569697e53711d7aff9
Vincent Kendrick <dagreatcode@gmail.com>
2022-10-05 09:09:53 -0400

    fixed cards for the profile page

--- client/src/containers/Profile/Profile.jsx
+++ client/src/containers/Profile/Profile.jsx
@@ -18,25 +18,35 @@ export default function Profile() {
       <h1 className="text-color">Profile</h1>
       {/* {projects.map((prod) => console.log(prod.title))} */}
       {/* <h1>{projects[0].title}</h1> */}
-      <div className="container" style={{}}>
+      <div className="container" >
         <div className="row">
           {projects.map((prod) => (
             <div
               key={prod._id}
               className="card"
-              style={{
+              
+              style={{margin: "auto;",                 maxWidth: "700px;",border: "2px solid white",
+                // border:"10px solid gray;",
+                height: "90vh;",
                 width: "18rem",
-                alignItems: "center;",
-                display: "flex;",
-                flexDirection: "row;",
-                textAlign: "center;",
+                color:"#FAEA48",
+                // alignItems: "center;",
+                // display: "flex;",
+                // flexDirection: "row;",
+                // textAlign: "center;",
+                // margin: "auto;",
+                display: "block;",
+                marginLeft: "auto;",
+                marginRight: "auto;",
+                backgroundColor:"#3330E4"
+                // width: "50%;",
               }}
             >
               <img src="..." className="card-img-top" alt="..." />
               <div className="card-body">
                 <h5 className="card-title">{prod.title}</h5>
                 <p className="card-text">{prod.description}</p>
-                <Link to="/" className="btn btn-primary">
+                <Link to="/" className="btn" style={{color:"#FAEA48",backgroundColor:"#F637EC"}}>
                   Check out Product
                 </Link>
               </div>
